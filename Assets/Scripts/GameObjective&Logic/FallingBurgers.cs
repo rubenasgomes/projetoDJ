2d242dd0c12464e34cd88aeb6275fd87f3ae7252
shecallmemadiba <106152472+shecallmemadiba@users.noreply.github.com>
2024-06-02 16:40:48 +0100

    script powerups

--- Assets/Scripts/GameObjective&Logic/FallingBurgers.cs
+++ Assets/Scripts/GameObjective&Logic/FallingBurgers.cs
@@ -8,18 +8,43 @@ public class FallingBurgers : MonoBehaviour
     public float spawnDelayBurgers = 3f;
     public float spawnDelaySpaceTrash = 3f;
     public float spawnDelayBatides = 3f;
+<<<<<<< Updated upstream
     public float fallingSpeedBurgers = 1f; // Velocidade dos hambúrgueres
     public float fallingSpeedSpaceTrash = 1f; // Velocidade do lixo espacial
     public float fallingSpeedBatides = 1f; // Velocidade dos "batides"
+=======
+    public float spawnDelayVelocidade = 3f;
+    public float spawnDelayCrescer = 3f;
+
+
+    public float fallingSpeedBurgers = 1f; // Velocidade dos hambúrgueres
+    public float fallingSpeedSpaceTrash = 1f; // Velocidade do lixo espacial
+    public float fallingSpeedBatides = 1f; // Velocidade dos "batides"
+    public float fallingSpeedVelocidade = 2f; // Velocidade dos "Velocidade"
+    public float fallingSpeedCrescer= 2f; // Velocidade dos "Crescer"
+
+
+>>>>>>> Stashed changes
     // Prefabs dos objetos
     public GameObject burgerPrefab;
     public GameObject spaceTrashPrefab;
     public GameObject batidesPrefab;
+<<<<<<< Updated upstream
+=======
+    public GameObject VelocidadePrefab;
+    public GameObject CrescerPrefab;
+>>>>>>> Stashed changes
     public GameObject spawnArea;
     private Vector3 spawnSize; // Tamanho da área de "spawn"
     public int simultaneousBurgers = 1; // Nº de hambúrgueres a "spawnar" simultâneamente
     public int simultaneousSpaceTrash = 1; // Nº de lixo espacial a "spawnar" simultâneamente
     public int simultaneousBatides = 1;  // Nº de "batides" a "spawnar" simultâneamente
+<<<<<<< Updated upstream
+=======
+    public int simultaneousVelocidade = 1;  // Nº de "Velocidade" a "spawnar" simultâneamente
+    public int simultaneousCrescer = 1;  // Nº de "Crescer" a "spawnar" simultâneamente
+
+>>>>>>> Stashed changes
     public Objective objectiveManager;
 
     void Start()
@@ -116,6 +141,63 @@ public class FallingBurgers : MonoBehaviour
             yield return new WaitForSeconds(spawnDelayBatides);
         }
     }
+<<<<<<< Updated upstream
+=======
+    // "Spawn" de "batides"/batidos
+    public IEnumerator SpawnVelocidade()
+    {
+        yield return new WaitForSeconds(spawnDelayVelocidade);
+
+        while (true)
+        {
+            for (int i = 0; i < simultaneousVelocidade; i++)
+            {
+                float randomX = Random.Range(-spawnSize.x / 2f, spawnSize.x / 2f);
+                float randomZ = Random.Range(-spawnSize.z / 2f, spawnSize.z / 2f);
+
+                Vector3 spawnPosition = spawnArea.transform.position + new Vector3(randomX, 0f, randomZ);
+                GameObject newBatide = Instantiate(batidesPrefab, spawnPosition, Quaternion.identity);
+
+                Rigidbody rb = newBatide.GetComponent<Rigidbody>();
+                if (rb == null)
+                {
+                    rb = newBatide.AddComponent<Rigidbody>();
+                }
+
+                rb.velocity = Vector3.down * fallingSpeedVelocidade;
+            }
+
+            yield return new WaitForSeconds(spawnDelayVelocidade);
+        }
+    }
+    // "Spawn" de "batides"/batidos
+    public IEnumerator spawnDelayCrescer()
+    {
+        yield return new WaitForSeconds(spawnDelayCrescer);
+
+        while (true)
+        {
+            for (int i = 0; i < simultaneousCrescer; i++)
+            {
+                float randomX = Random.Range(-spawnSize.x / 2f, spawnSize.x / 2f);
+                float randomZ = Random.Range(-spawnSize.z / 2f, spawnSize.z / 2f);
+
+                Vector3 spawnPosition = spawnArea.transform.position + new Vector3(randomX, 0f, randomZ);
+                GameObject newBatide = Instantiate(batidesPrefab, spawnPosition, Quaternion.identity);
+
+                Rigidbody rb = newBatide.GetComponent<Rigidbody>();
+                if (rb == null)
+                {
+                    rb = newBatide.AddComponent<Rigidbody>();
+                }
+
+                rb.velocity = Vector3.down * fallingSpeedCrescer;
+            }
+
+            yield return new WaitForSeconds(spawnDelayCrescer);
+        }
+    }
+>>>>>>> Stashed changes
 
     // Acabar com o "spawn" de vez
     public void StopSpawn()
@@ -130,6 +212,11 @@ public class FallingBurgers : MonoBehaviour
         GameObject[] burgers = GameObject.FindGameObjectsWithTag("Hamburger");
         GameObject[] trash = GameObject.FindGameObjectsWithTag("Trash");
         GameObject[] batides = GameObject.FindGameObjectsWithTag("Batides");
+<<<<<<< Updated upstream
+=======
+        GameObject[] velocidade = GameObject.FindGameObjectsWithTag("Velocidade");
+        GameObject[] crescer = GameObject.FindGameObjectsWithTag("Crescer");
+>>>>>>> Stashed changes
 
         foreach (GameObject burger in burgers)
         {
@@ -145,21 +232,50 @@ public class FallingBurgers : MonoBehaviour
         {
             Destroy(batide);
         }
+<<<<<<< Updated upstream
     }
 
     // Método para mudar o rating do "spawn"
     public void ChangeSpawnRates(float newBurgerSpawnDelay, float newSpaceTrashSpawnDelay, float newBatidesSpawnDelay)
+=======
+        foreach (GameObject velocida in velocidade)
+        {
+            Destroy(velocida);
+        }
+        foreach (GameObject cresce in crescer)
+        {
+            Destroy(cresce);
+        }
+    }
+
+    // Método para mudar o rating do "spawn"
+    public void ChangeSpawnRates(float newBurgerSpawnDelay, float newSpaceTrashSpawnDelay, float newBatidesSpawnDelay, float newVelocidadeSpawnDelay, float newCrescerSpawnDelay)
+>>>>>>> Stashed changes
     {
         spawnDelayBurgers = newBurgerSpawnDelay;
         spawnDelaySpaceTrash = newSpaceTrashSpawnDelay;
         spawnDelayBatides = newBatidesSpawnDelay;
+<<<<<<< Updated upstream
     }
 
     // Método para mudar os "spawns" simultâneos
     public void ChangeSimultaneousSpawns(int newSimultaneousBurgers, int newSimultaneousSpaceTrash, int newSimultaneousBatides)
+=======
+        spawnDelayVelocidade = newVelocidadeSpawnDelay;
+        spawnDelayCrescer = newCrescerSpawnDelay;
+    }
+
+    // Método para mudar os "spawns" simultâneos
+    public void ChangeSimultaneousSpawns(int newSimultaneousBurgers, int newSimultaneousSpaceTrash, int newSimultaneousBatides, int newSimultaneousVelocidade, int newSimultaneousCrescer)
+>>>>>>> Stashed changes
     {
         simultaneousBurgers = newSimultaneousBurgers;
         simultaneousSpaceTrash = newSimultaneousSpaceTrash;
         simultaneousBatides = newSimultaneousBatides;
+<<<<<<< Updated upstream
+=======
+        simultaneousVelocidade = newSimultaneousVelocidade;
+        simultaneousCrescer = newSimultaneousCrescer;
+>>>>>>> Stashed changes
     }
 }
